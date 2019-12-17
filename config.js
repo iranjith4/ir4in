de31da239f89ca8075d670866c267473c4accc84
Ranjithkumar Matheswaran <r@ir4.in>
2019-12-17 21:06:48 +0530

    URLs changes

--- config.js
+++ config.js
@@ -30,19 +30,19 @@ module.exports = {
     bio: 'Software Engineer / Traveler. Working as SDE Mobile at RadiusAgent. Swift | Objective C | iOS | Flutter',
     contacts: {
       email: 'r@ir4.in',
-      facebook: 'https://fb.com/iranjith4',
+      facebook: 'iranjith4',
       telegram: '#',
-      twitter: 'https://twitter.com/iranjith4',
-      github: 'https://github.com/iranjith4',
+      twitter: 'iranjith4',
+      github: 'iranjith4',
       rss: '',
       vkontakte: '',
-      linkedin: 'https://in.linkedin.com/in/iranjith4',
-      instagram: 'https://instagram.com/iranjith4',
+      linkedin: 'iranjith4',
+      instagram: 'iranjith4',
       line: '',
       gitlab: '',
       weibo: '',
       codepen: '',
-      youtube: 'https://youtube.com/iranjith4'
+      youtube: 'iranjith4'
     }
   }
 };
