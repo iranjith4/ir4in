1e9e01d4cb38a9dfb72e2db331c7380f042ecf29
Ranjithkumar Matheswaran <r@ir4.in>
2019-12-17 17:04:01 +0530

    Analytics added

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-61718268-1',
   useKatex: false,
   menu: [
     {
