67eac72536fb1199b8dca9986a18530b23d84995
Ranjithkumar Matheswaran <r@ir4.in>
2020-01-06 17:41:06 +0530

    Site ready to deploy

--- config.js
+++ config.js
@@ -4,38 +4,38 @@ module.exports = {
   url: 'https://ir4.in',
   pathPrefix: '/',
   title: 'Ranjithkumar Matheswaran',
-  subtitle: 'Senior Software Engineer - iOS, Radius Agent. Self thaught UI/UX Designer. Believes in Design + Engineering = Perfect product. \n \n Specialised in Swift, Obj C, iOS, Xcode, Analytics, Flutter, Sketch, Adobe XD, UI/UX, User Research.',
+  subtitle: 'Lead Software Engineer - iOS \n \n Self thaught UI/UX Designer. Believes in Design + Engineering = Perfect product. \n \n Specialised in Swift, Obj C, iOS, Xcode, Analytics, Flutter, Sketch, Adobe XD, UI/UX, User Research.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
-  postsPerPage: 4,
+  postsPerPage: 2,
   googleAnalyticsId: 'UA-61718268-1',
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
+      label: 'About',
       path: '/'
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: 'Works',
+      path: '/pages/mywork'
     },
     {
       label: 'Contact me',
       path: '/pages/contacts'
     },
     {
-      label: 'My work',
-      path: '/pages/mywork'
+      label: 'Travel',
+      path: '/pages/travel'
     }
   ],
   author: {
     name: 'Ranjithkumar Matheswaran',
     photo: '/photo.jpg',
-    bio: 'Senior Software Engineer - iOS, Radius Agent. Self thaught UI/UX Designer. Believes in Design + Engineering = Perfect product. \n \n Specialised in Swift, Obj C, iOS, Xcode, Analytics, Flutter, Sketch, Adobe XD, UI/UX, User Research.',
+    bio: 'Lead Software Engineer - iOS \n \n Self thaught UI/UX Designer. Believes in Design + Engineering = Perfect product. \n \n Specialised in Swift, Obj C, iOS, Xcode, Analytics, Flutter, Sketch, Adobe XD, UI/UX, User Research.',
     contacts: {
       email: 'r@ir4.in',
-      facebook: 'iranjith4',
-      telegram: 'iranjith4',
+      facebook: '',
+      telegram: '',
       twitter: 'iranjith4',
       github: 'iranjith4',
       rss: '',
